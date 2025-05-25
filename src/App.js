50647df6534d0b4454b34721d648a0c26a786057
Parneet Singh <parneet.singh@ukg.com>
2025-05-25 20:28:46 +0530

    netflix-gpt

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="bg-blue-500 text-white text-center p-10 rounded-lg shadow-lg">
+      Tailwind CSS is working with CRA! 🚀
+    </div>
     </div>
   );
 }
